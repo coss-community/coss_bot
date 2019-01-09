68013a63b5414a02cf4ef9aeed53062026008c8e
Viktor Vsk <ablebeam@gmail.com>
2019-01-09 22:00:11 +0200

    Fix small issue related to absence of ActiveRecord; Add 'Bearish' bot; bump version

--- lib/coss_bot.rb
+++ lib/coss_bot.rb
@@ -3,6 +3,7 @@
 require 'coss_bot/version'
 require 'coss_bot/basic'
 require 'coss_bot/buy_low_sell_high'
+require 'coss_bot/sell_high_buy_low'
 require 'benchmark'
 require 'logger'
 require 'coss_api_ruby_wrapper'
--- lib/coss_bot/sell_high_buy_low.rb
+++ lib/coss_bot/sell_high_buy_low.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 module CossBot
-  class BuyLowSellHigh < Basic
+  class SellHighBuyLow < Basic
     private
 
     def tick
@@ -14,18 +14,18 @@ module CossBot
       currency_to_sell = pair.split('_').last
       current_balance = balances.detect { |c| c['currency_code'] == currency_to_sell }['available'].to_f
       pair_depth = exchange.pair_depth(pair)
-      current_price = pair_depth['asks'].first.first.to_f
+      current_price = pair_depth['bids'].first.first.to_f
 
       if current_balance <= trade_limit.to_f
         yield(nil, nil, { status: 0, message: 'Current balance <= trade limit' })
       elsif current_balance <= current_price * lot_size
         yield(nil, nil, { status: 0, message: 'Current balance <= current_price * lot_size' })
       else
-        price_with_profit = (current_price + (current_price / 100 * profit.to_f)).round(6)
-        logger.info("Placing BUY order for #{current_price}. Lot size: #{lot_size}")
-        buy_response = exchange.place_limit_order(pair, current_price, 'BUY', lot_size)
-        logger.info("Placing SELL order for #{price_with_profit}. Lot size: #{lot_size}")
-        sell_response = exchange.place_limit_order(pair, price_with_profit, 'SELL', lot_size)
+        price_with_profit = (current_price - (current_price / 100 * profit.to_f)).round(6)
+        logger.info("Placing SELL order for #{current_price}. Lot size: #{lot_size}")
+        sell_response = exchange.place_limit_order(pair, current_price, 'SELL', lot_size)
+        logger.info("Placing BUY order for #{price_with_profit}. Lot size: #{lot_size}")
+        buy_response = exchange.place_limit_order(pair, price_with_profit, 'BUY', lot_size)
         logger.info('=== End of trading cycle ===')
 
         error = {}
